eb5b7512615594ced57bbaea029dc56e87f3dac2
ALOK <ALOK@DESKTOP-RMQ93UK>
2023-12-02 13:18:47 +0530

    Add project files.

---
+++ HackerNewsApi/Models/Story.cs
@@ -0,0 +1,8 @@
+﻿namespace HackerNewsApi.Models
+{
+    public class Story
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+    }
+}
---
+++ HackerNewsApi/Services/StoryService.cs
@@ -0,0 +1,39 @@
+﻿using HackerNewsApi.Models;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace HackerNewsApi.Services
+{
+    public interface IStoryService
+    {
+        IEnumerable<Story> GetNewestStories(int pageSize, int page);
+        IEnumerable<Story> SearchStories(string query);
+    }
+
+    public class StoryService : IStoryService
+    {
+        private readonly HttpClient _httpClient;
+
+        public StoryService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public IEnumerable<Story> GetNewestStories(int pageSize, int page)
+        {
+            // Fetch data from Hacker News API
+            // Implement caching logic if needed
+            // For demonstration purposes, returning an empty list
+            return new List<Story>();
+        }
+
+        public IEnumerable<Story> SearchStories(string query)
+        {
+            // Fetch data from Hacker News API based on search query
+            // For demonstration purposes, returning an empty list
+            return new List<Story>();
+        }
+
+    }
+
+}
