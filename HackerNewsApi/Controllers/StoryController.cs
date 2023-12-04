59520c18ac4349afa930d0ef7fa05217b94769c3
alokdubey71 <46102237+alokdubey71@users.noreply.github.com>
2023-12-04 08:47:20 +0530

    #stroy id : add newton soft package and changes

--- HackerNewsApi/Controllers/StoryController.cs
+++ HackerNewsApi/Controllers/StoryController.cs
@@ -1,33 +1,29 @@
-﻿using HackerNewsApi.Services;
-using Microsoft.AspNetCore.Http;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace HackerNewsApi.Controllers
+[Route("api/[controller]")]
+[ApiController]
+public class StoryController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class StoryController : ControllerBase
+    private readonly IStoryService _storyService;
+
+    public StoryController(IStoryService storyService)
     {
-        private readonly IStoryService _storyService;
-
-        public StoryController(IStoryService storyService)
-        {
-            _storyService = storyService;
-        }
-
-        [HttpGet]
-        public IActionResult GetNewestStories(int pageSize = 10, int page = 1)
-        {
-            var stories = _storyService.GetNewestStories(pageSize, page);
-            return Ok(stories);
-        }
-
-        [HttpGet("search")]
-        public IActionResult SearchStories(string query)
-        {
-            var result = _storyService.SearchStories(query);
-            return Ok(result);
-        }
+        _storyService = storyService;
     }
 
+    [HttpGet]
+    public async Task<IActionResult> GetNewestStories(int pageSize = 10, int page = 1)
+    {
+        var stories = await _storyService.GetNewestStories(pageSize, page);
+        return Ok(stories);
+    }
+
+    [HttpGet("search")]
+    public async Task<IActionResult> SearchStories(string query)
+    {
+        var result = await _storyService.SearchStories(query);
+        return Ok(result);
+    }
 }
--- HackerNewsApi/Services/StoryService.cs
+++ HackerNewsApi/Services/StoryService.cs
@@ -1,39 +1,53 @@
-﻿using HackerNewsApi.Models;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using HackerNewsApi.Models;
+using Microsoft.Extensions.Caching.Memory;
+using Newtonsoft.Json;
 
-namespace HackerNewsApi.Services
+public interface IStoryService
 {
-    public interface IStoryService
+    Task<IEnumerable<Story>> GetNewestStories(int pageSize, int page);
+    Task<IEnumerable<Story>> SearchStories(string query);
+}
+public class StoryService : IStoryService
+{
+    private readonly HttpClient _httpClient;
+
+    public StoryService(HttpClient httpClient)
     {
-        IEnumerable<Story> GetNewestStories(int pageSize, int page);
-        IEnumerable<Story> SearchStories(string query);
+        _httpClient = httpClient;
     }
 
-    public class StoryService : IStoryService
+    public async Task<IEnumerable<Story>> GetNewestStories(int pageSize, int page)
     {
-        private readonly HttpClient _httpClient;
+        var response = await _httpClient.GetStringAsync($"newstories.json?print=pretty");
+        var storyIds = JsonConvert.DeserializeObject<IEnumerable<int>>(response);
 
-        public StoryService(HttpClient httpClient)
+        var stories = new List<Story>();
+
+        foreach (var storyId in storyIds.Skip((page - 1) * pageSize).Take(pageSize))
         {
-            _httpClient = httpClient;
+            var story = await GetStoryById(storyId);
+            stories.Add(story);
         }
 
-        public IEnumerable<Story> GetNewestStories(int pageSize, int page)
-        {
-            // Fetch data from Hacker News API
-            // Implement caching logic if needed
-            // For demonstration purposes, returning an empty list
-            return new List<Story>();
-        }
-
-        public IEnumerable<Story> SearchStories(string query)
-        {
-            // Fetch data from Hacker News API based on search query
-            // For demonstration purposes, returning an empty list
-            return new List<Story>();
-        }
-
+        return stories;
     }
 
+    public async Task<IEnumerable<Story>> SearchStories(string query)
+    {
+        // This is a simplified example. You would need to implement the actual search logic.
+        throw new NotImplementedException();
+    }
+
+    private async Task<Story> GetStoryById(int storyId)
+    {
+        var response = await _httpClient.GetStringAsync($"item/{storyId}.json?print=pretty");
+        var story = JsonConvert.DeserializeObject<Story>(response);
+        return story;
+    }
 }
